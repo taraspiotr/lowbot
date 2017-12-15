0b4ea825d8d8e25c2586e53132330bed8c0107d1
Piotr Tarasiewicz <taras.piotr@gmail.com>
2017-12-15 19:17:08 +0100

    Initial commit

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+lowbot AI for poker
+===================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ lowbot/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'lowbot'
+project = "lowbot AI for poker"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'It plays poker'
+authors = ['Piotr Tarasiewicz']
+authors_string = ', '.join(authors)
+emails = ['taras.piotr@gmail.com']
+license = 'MIT'
+copyright = '2017 ' + authors_string
+url = 'http://github.com/taraspiotr/lowbot'
